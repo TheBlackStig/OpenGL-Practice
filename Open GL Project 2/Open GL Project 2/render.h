2ea1e0b49e95a450b55a63bcb247cc7d9c1027d0
TheBlackStig <th3.black.st1g@gmail.com>
2017-09-11 11:57:10 +0100

    Random unresolved externals

---
+++ Open GL Project 2/Open GL Project 2/render.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//Standard libraries
+#include <iostream>
+
+//Graphics libraries
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
+int main();
+void framebuffer_size_callback(GLFWwindow*, int width, int height);
+
+	
