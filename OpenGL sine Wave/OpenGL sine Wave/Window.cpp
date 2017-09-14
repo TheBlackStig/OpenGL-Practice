8ebb2ada459aedfde14f6f6d2c1941bfa3419bab
TheBlackStig <th3.black.st1g@gmail.com>
2017-09-14 12:15:12 +0100

    init fro window, glfw, glad need to get window render set up

---
+++ OpenGL sine Wave/OpenGL sine Wave/Window.cpp
@@ -0,0 +1,32 @@
+#include "Window.h"
+
+int main()
+{
+	//Screen width and height 
+	const unsigned int SCR_WIDTH = 800;
+	const unsigned int SCR_HEIGHT = 600;
+	const char WINDOW_NAME[] = "Sine Wave Render";
+
+	//InstantiateGLFW window 
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//	Construct window object
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, WINDOW_NAME, NULL, NULL);
+	//Check for window initilisation
+	if (window == NULL)
+	{
+		std::cout << "FAILED TO CREATE WINDOW." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	//Initilisation of GLAD
+	if (!gladLoadGLLoader((GLADloadProc)glfwGetProcAddress));
+	{
+
+	}
+	return 0;
+}
--- OpenGL sine Wave/OpenGL sine Wave/Window.h
+++ OpenGL sine Wave/OpenGL sine Wave/Window.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <iostream>
+
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
+int main();
+
+
