82f1b67970b00e282c058ff6411eaec0cecc22df
TheBlackStig <th3.black.st1g@gmail.com>
2017-09-26 09:19:19 +0100

    Fuck it

--- SineWave with better modularisation/SineWave with better modularisation/mainWindow.h
+++ SineWave with better modularisation/SineWave with better modularisation/mainWindow.h
@@ -1,10 +1,12 @@
 #pragma once
 
 //Rendering libraries
-#include <GLFW\glfw3.h>
 #include <glad\glad.h>
-//standard stream libraries
+#include <GLFW\glfw3.h>
+
+//standard libraries
 #include <iostream>
+#include <vector>
 
 void processInput(GLFWwindow* window);
 void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
