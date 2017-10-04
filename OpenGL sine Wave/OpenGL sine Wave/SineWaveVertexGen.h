da840aa5bc31ba0a5589108e8331bf02ea0370b8
TheBlackStig <th3.black.st1g@gmail.com>
2017-10-04 10:24:58 +0100

    sinewave still not rendering on whole screen

--- OpenGL sine Wave/OpenGL sine Wave/SineWaveVertexGen.h
+++ OpenGL sine Wave/OpenGL sine Wave/SineWaveVertexGen.h
@@ -4,10 +4,13 @@
 
 //Basic includes
 #include <iostream>
+#include <iomanip>
 #include "math.h"
 #include <vector>
 
 //Function to return vector of 2D coords with enough coords to create a curve
 std::vector<float> sineCurveGeneration();
+//Procedure to display the contents of a vector in the console
+void vectorDisplay(std::vector<float> input_vector);
 
 
--- OpenGL sine Wave/OpenGL sine Wave/Window.cpp
+++ OpenGL sine Wave/OpenGL sine Wave/Window.cpp
@@ -7,10 +7,10 @@
 #include <iostream>
 
 
-// settings
+//Window settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
+//Shader source code
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
@@ -22,24 +22,26 @@ const char *fragmentShaderSource = "#version 330 core\n"
 "void main()\n"
 "{\n"
 "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
+"}\0";
+
+const char *blue_frag_shader = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(0.423f, 0.964f, 0.352f, 1.0f);\n"
+"}\0";
 
 int main()
 {
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//GLFW initialize and configure
+	glfwInit();														//Inital intantiasation od object
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);					//Version of GLFW major
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);					//Version of GLFW minor
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	//Core profile version
 
-#ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-#endif
-
-														 // glfw window creation
-														 // --------------------
+	//GLFW window creation using constants SCR_WIDTH and SCR_HEIGHT
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	//Checks for successful window creation
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -47,10 +49,10 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	//Checks of OS has chanegd veiwport size on start
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
+	//Load all OpenGL function pointers with GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
@@ -58,40 +60,52 @@ int main()
 	}
 
 
-	// build and compile our shader program
-	// ------------------------------------
-	// fragment shader
-	unsigned int vertexShader = vertexShaderCompiler(vertexShaderSource);
-	int success;
-	char infoLog[512];
-	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	// check for shader compile errors
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
+	//Build and compile shader program for sinewave
+	//Vertex shader compilation
+	unsigned int vertex_shader = vertexShaderCompiler(vertexShaderSource);
+	//Fragment shader compilation
+	unsigned int sine_fragment_shader = fragmentShaderCompiler(fragmentShaderSource);
 	// link shaders
-	int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	// check for linking errors
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	int success;
+	char info_log[512];
+	int shader_program = glCreateProgram();
+	//Attach the vertex shader to the sine wave shader program
+	glAttachShader(shader_program, vertex_shader);
+	//Attach the fragment shader for the sine wave shader program
+	glAttachShader(shader_program, sine_fragment_shader);
+	//Link the two shaders together to form a single shader
+	glLinkProgram(shader_program);
+	//Check for linking errors
+	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
 	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info_log << std::endl;
 	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	//Vertex shader isnt deleted yet as it is used next
+	glDeleteShader(sine_fragment_shader);
 
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------
+	//Build and compile shader program for axis
+	//Note that the vertex shader from the previous is used as that can be identical
+	unsigned int axis_fragment_shader = fragmentShaderCompiler(blue_frag_shader);
+	int axis_shader_program = glCreateProgram();
+	//Attach the vertex shader to the axis shader program
+	glAttachShader(axis_shader_program, vertex_shader);
+	//Attach the axis fragment shader to the axis shader program
+	glAttachShader(axis_shader_program, axis_fragment_shader);
+	//Link the two shaders together to form a single shader 
+	glLinkProgram(axis_shader_program);
+	//Check for linking errors
+	glGetProgramiv(axis_shader_program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(axis_shader_program, 512, NULL, info_log);
+		std::cout << "ERROR::AXIS::SHADER::PROGRAM::LINKING_FAILED\n" << info_log << std::endl;
+	}
+
+	//Set up vertex data (and buffer(s)) and configure vertex attributes
 	//Sinewave
 	std::vector<float> vertices = sineCurveGeneration();
+	//Displays vector sine wave
+	vectorDisplay(vertices);
 	//Penguin attempt
 	/*float vertices[] = {-0.04f,0.8f,
 		0.04f,0.8f,
@@ -129,114 +143,139 @@ int main()
 		0, 1, 3,  // first Triangle
 		1, 2, 3   // second Triangle
 	};*/
-	//unsigned int VBO, VAO, EBO;
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	//glGenBuffers(1, &EBO);
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//For vectors
-	glBufferData(GL_ARRAY_BUFFER, vertices.size(), &vertices[0], GL_STATIC_DRAW);
-	//For arrays
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	glBindVertexArray(0);
+	//Axis coords
+	float axis[] = { -1.0f, 0.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		0.0f, -1.0f };
 
 
-	// uncomment this call to draw in wireframe polygons.
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	///Buffer setup
+	unsigned int VBO[2], VAO[2];
+	//Generate 2 Vertex Attribute Objects
+	glGenVertexArrays(2, VAO);
+	//Generate 2 Vertex Buffer Objects
+	glGenBuffers(2, VBO);
+	
 
-	// render loop
-	// -----------
+	///For First set of VAO and VBO for sine wave
+	glBindVertexArray(VAO[0]);														//Binding first VAO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);											//Binding first VBO
+	glBufferData(GL_ARRAY_BUFFER, vertices.size(), &vertices[0], GL_STATIC_DRAW);	//Binding vertex data to the buffer 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	//Telling opengl how to interpret the data
+																					//In this case it is in 3s x,y,z
+	glEnableVertexAttribArray(0);													// enabling the buffer
+
+	///For second set of VAO and VBO for axis
+	glBindVertexArray(VAO[1]);														// Binding second VAO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);											// Binding second VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(axis), axis, GL_STATIC_DRAW);				//Binding vertex data to the buffer
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);	//Telling opengl to interpret vertex as x,y
+	glEnableVertexAttribArray(0);													//enabling the buffer
+
+	//Allows any shapes to be drawn in a line mode 
+	//As only lines are being drawn this isn't needed
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	//The render loop
 	while (!glfwWindowShouldClose(window))
 	{
-		// input
-		// -----
+		//Check for inputs
 		processInput(window);
 
-		// render
-		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		//Render the screen with background color of black
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+	
+		///Draw sinewave
+		//Set width of line
+		glLineWidth(1);
+		//Tell openGL which shader program to use
+		glUseProgram(shader_program);
+		//Binding the VAO to the sine wave 
+		glBindVertexArray(VAO[0]);
+		//Draw the sinewave with the given 3D coords
+		glDrawArrays(GL_POINTS, 0, vertices.size());
 
-		// draw our first triangle
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		//For Arrays
-		//glDrawArrays(GL_LINE_STRIP, 0, sizeof(vertices));
-		//For Vectors
-		glDrawArrays(GL_LINE_STRIP, 0, vertices.size());
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		// glBindVertexArray(0); // no need to unbind it every time 
+
+		///Drawing axis
+		//Set wdith of lines
+		glLineWidth(2);
+		//Tell openGL which shader program to use
+		glUseProgram(axis_shader_program);
+		//Binding VAO to axis array 
+		glBindVertexArray(VAO[1]); 
+		//Drawing axis
+		glDrawArrays(GL_LINE_STRIP, 0, sizeof(axis));
 
 
-
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
+		//Swap the buffers for the window ready for next frame render
 		glfwSwapBuffers(window);
+		//Poll the events (e.g. button clicks)
 		glfwPollEvents();
 	}
 
-	// optional: de-allocate all resources once they've outlived their purpose:
-	// ------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	//glDeleteBuffers(1, &EBO);
+	//Deallocate all resources once they are finished with
+	glDeleteVertexArrays(2, VAO);
+	glDeleteBuffers(2, VBO);
 
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
+	//Terminate all glfw processes and data
 	glfwTerminate();
 	return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+//Process all inputs: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+//Whenever the window size changed (by OS or user resize) this callback function executes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	// make sure the viewport matches the new window dimensions; note that width and 
-	// height will be significantly larger than specified on retina displays.
+	//Make sure the viewport matches the new window dimensions; note that width and 
+	//Height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
 }
 
 unsigned int vertexShaderCompiler(const char* shader_source)
 {
-	// build and compile our shader program
-	// ------------------------------------
-	// vertex shader
-	int shader_compiled = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(shader_compiled, 1, &shader_source, NULL);
-	glCompileShader(shader_compiled);
+	//Build and compile shader program
+	//Vertex shader
+	int vertex_shader_compiled = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader_compiled, 1, &shader_source, NULL);
+	glCompileShader(vertex_shader_compiled);
+	//Check for shader compile errors
+	int success;
+	char info_log[512];
+	glGetShaderiv(vertex_shader_compiled, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertex_shader_compiled, 512, NULL, info_log);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << std::endl;
+	}
+	return vertex_shader_compiled;
+}
+
+unsigned int fragmentShaderCompiler(const char* shader_source)
+{
+	//Build and compile shader program
+	//Fragment shader
+	
+	int fragment_shader_compiled = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader_compiled, 1, &shader_source, NULL);
+	glCompileShader(fragment_shader_compiled);
 	// check for shader compile errors
 	int success;
 	char info_log[512];
-	glGetShaderiv(shader_compiled, GL_COMPILE_STATUS, &success);
+	glGetShaderiv(fragment_shader_compiled, GL_COMPILE_STATUS, &success);
 	if (!success)
 	{
-		glGetShaderInfoLog(shader_compiled, 512, NULL, info_log);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << std::endl;
+		glGetShaderInfoLog(fragment_shader_compiled, 512, NULL, info_log);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << info_log << std::endl;
 	}
-	return shader_compiled;
+
+	return fragment_shader_compiled;
 }
