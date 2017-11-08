3498b024e1b09b8ccff29d6c672c8b3fc3e8cf1f
TheBlackStig <th3.black.st1g@gmail.com>
2017-11-08 09:43:52 +0000

    Started menu practice

---
+++ Menu Practice/Menu Practice/Window.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+//Includes
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
+#include <iostream>
+
+//function definitions
+void frameBufferSizeCallBack(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
