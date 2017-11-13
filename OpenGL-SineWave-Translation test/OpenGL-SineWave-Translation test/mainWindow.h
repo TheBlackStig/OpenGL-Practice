da09a1062d2f88519f9e11c0ef24c88d2a5a28c2
TheBlackStig <th3.black.st1g@gmail.com>
2017-11-13 15:32:21 +0000

    About to start tranformations

--- OpenGL-SineWave-Translation test/OpenGL-SineWave-Translation test/mainWindow.h
+++ OpenGL-SineWave-Translation test/OpenGL-SineWave-Translation test/mainWindow.h
@@ -7,6 +7,11 @@
 #include <glad\glad.h>
 #include <GLFW\glfw3.h>
 
+//transformation libraries
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
 const int SCR_WIDTH = 800;
 const int SCR_HEIGHT = 600;
 
